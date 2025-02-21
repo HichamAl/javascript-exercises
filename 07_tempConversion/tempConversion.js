9a503720093dabef70631657e0819e0bdcca7439
Beghdad Almakroudi <beghdadalmakroudi@MacBook-Pro-van-Beghdad.local>
2025-02-21 11:06:45 +0100

    finished until 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let temp = (tempInFahrenheit - 32) * 5/9;
+  let roundedTemp = Math.round(temp * 10) / 10;
+  return roundedTemp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  let temp = tempInCelcius * 9/5 + 32;
+  let roundedTemp = Math.round(temp * 10) / 10;
+  return roundedTemp
 };
 
 // Do not edit below this line
