bc986ffee83127f0d268045c3da8a2b5117ff194
Beghdad Almakroudi <beghdadalmakroudi@MacBook-Pro-van-Beghdad.local>
2025-02-24 19:35:31 +0100

    finish 08 calc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1+number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1-number2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  // reduce for function where you 
+	const sum = array.reduce((totalSum, arraynumber) => totalSum + arraynumber, 0);
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  // reduce function again for multiplying numbers in array iwth eachtother
+
+  // initial value is the first value in array...
+  const result = array.reduce((total, arrayNumber) => total * arrayNumber)
+  return result
 
 };
 
-const power = function() {
-	
+const power = function(number, powernumber) {
+  const result = Math.pow(number, powernumber);
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let product = 1;
+  for (let i = number; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+
 };
 
 // Do not edit below this line
